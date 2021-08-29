7a352860ecd2218fb55894754e0258cb63d03527
Antonio Pérez <antperdev@gmail.com>
2021-08-29 12:44:48 +0200

    formularios reactivos básicos, dinamicos y switches

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent   {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3),],],
+    favoritos: this.fb.array( [
+        ['Metal Gear', Validators.required ],
+      ['The Stranding', Validators.required ]
+    ] ,Validators.required  )
+  });
 
-  ngOnInit(): void {
+
+  // nuevoFavorito: FormControl = new FormControl;
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+get favoritosArr() {
+  return this.miFormulario.get('favoritos') as FormArray;
+}
+
+  constructor(private fb: FormBuilder) { }
+
+  campoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors
+      &&
+      this.miFormulario.controls[campo].touched
+  }
+
+  agregarFavorito(){
+    if (this.nuevoFavorito.invalid){return;}
+
+    // this.favoritosArr.push(new FormControl(this.nuevoFavorito.value, Validators.required));
+    this.favoritosArr.push(this.fb.control( this.nuevoFavorito.value, Validators.required  )  );
+
+    this.nuevoFavorito.reset();
+
+  }
+
+
+  borrar(index:number){
+    this.favoritosArr.removeAt(index);
+  }
+
+  guardar(){
+    if (this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    //imprimir valor del formularios solo si es valido
+    //deberia imprimir todos los errores que se produzcan
+
+    console.log(this.miFormulario.value);
+
   }
 
 }
